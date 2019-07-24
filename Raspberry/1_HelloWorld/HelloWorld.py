00d647a2931f429477c23da1a8adc50ab644a3c5
victorolave <victorolave1131@gmail.com>
2019-07-24 16:43:26 -0500

    Updated Raspi Sample

--- Raspberry/1_HelloWorld/HelloWorld.py
+++ Raspberry/1_HelloWorld/HelloWorld.py
@@ -2,8 +2,16 @@ import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BOARD)
-gpio.setup(12, gpio.OUT)
+
+gpio.setup(24, gpio.OUT)
+gpio.setup(23, gpio.IN)
+
 gpio.output(12, True)
-time.sleep(20)
-gpio.output(12, False)
-gpio.cleanup(12)
+
+while True:
+    input_state == GPIO.inpu(23)
+
+    if input_state == False:
+        gpio.output(24, True)
+    else
+        gpio.output(24, False)
